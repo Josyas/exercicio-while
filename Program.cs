cdb09cf44d5fbb531f89ff0b8b1f7d07a1606c33
Josias <87546094+Josyas@users.noreply.github.com>
2021-08-21 23:41:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio_while {
+    class Program {
+        static void Main(string[] args) {
+            
+
+            Console.WriteLine("1 Álcool ");
+            Console.WriteLine("2 Gasolina ");
+            Console.WriteLine("3 Diesel ");
+            Console.WriteLine("4 Sair ");
+            Console.WriteLine("Escolha uma opção: ");
+            int escolha = int.Parse(Console.ReadLine());
+           
+
+            int cont1 = 0;
+            int cont2 = 0;
+            int cont3 = 0;
+            while (escolha != 4) {
+                
+                if (escolha == 1) {
+                    Console.WriteLine("Álcool");
+                    cont1++;
+                }
+                else if (escolha == 2) {
+                    Console.WriteLine("Gasolina");
+                    cont2++;
+                } else {
+                    Console.WriteLine("Diesel");
+                    cont3++;
+                }
+                Console.WriteLine("Escolha outra opção: ");
+                escolha = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Muito obrigado!!!");
+            Console.WriteLine($"Álcool {cont1}");
+            Console.WriteLine($"Gasolina {cont2}");
+            Console.WriteLine($"Diesel {cont3}");
+            
+
+        }
+    }
+}
